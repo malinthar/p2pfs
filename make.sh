e9a56377ac1fd097e6aaf061a7e41464e138c5a5
Chanuka Dissanayaka <chanuka04heshan@gmail.com>
2021-04-21 00:17:00 +0530

    Makefile changes

--- make.sh
+++ make.sh
@@ -1,3 +1,12 @@
+random1=$(( ( RANDOM % 10 )  + 1 ))
+random2=$(( ( RANDOM % 10 )  + 1 ))
+random3=$(( ( RANDOM % 10 )  + 1 ))
+
+echo $random1
+echo $random2
+echo $random3
+
+
 for i in {1..10}
   do
 	gnome-terminal --tab -- /bin/bash -c "java -jar target/io.viro.p2pfs-1.0-SNAPSHOT.jar 127.0.0.${i} 5000${i} node${i} localhost 55555"
